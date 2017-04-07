386a4c9988f00ceb0a56f4909518c97b247545dd
andrewdickens <dickens.a@husky.neu.edu>
2017-04-06 21:41:07 -0700

    updates

--- assignment/models/model.server.js
+++ assignment/models/model.server.js
@@ -3,9 +3,9 @@ module.exports = function () {
 
     var mongoose = require('mongoose');
    // mongoose.connect('mongodb://localhost/DistSystems-Spring-2017');
-     mongoose.connect('mongodb://user:user@ds137149.mlab.com:37149/distsystemsspring2017');
+   //   mongoose.connect('mongodb://user:user@ds137149.mlab.com:37149/distsystemsspring2017');
 
-    // mongoose.connect('mongodb://root:password@ec2-54-218-121-32.us-west-2.compute.amazonaws.com');
+    mongoose.connect('mongodb://root:mCjEkPqFmdk6@ec2-54-191-160-5.us-west-2.compute.amazonaws.com');
 
     var userModel = require("./user/user.model.server")();
     var productModel = require("./product/product.model.server")();
