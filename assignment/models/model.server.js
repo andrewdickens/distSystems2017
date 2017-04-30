9b46c16bf83d2f30a06634c3140b1a50237abebc
andrewdickens <dickens.a@husky.neu.edu>
2017-04-30 11:52:23 -0700

    updates to remote mongo

--- assignment/models/model.server.js
+++ assignment/models/model.server.js
@@ -2,10 +2,10 @@ module.exports = function () {
     console.log("in model.server");
 
     var mongoose = require('mongoose');
-   mongoose.connect('mongodb://localhost/DistSystems-Spring-2017');
+   // mongoose.connect('mongodb://localhost/DistSystems-Spring-2017');
    //   mongoose.connect('mongodb://user:user@ds137149.mlab.com:37149/distsystemsspring2017');
 
-    // mongoose.connect('mongodb://root:mCjEkPqFmdk6@ec2-54-191-160-5.us-west-2.compute.amazonaws.com');
+    mongoose.connect('mongodb://root:mCjEkPqFmdk6@ec2-54-202-98-117.us-west-2.compute.amazonaws.com');
 
     var userModel = require("./user/user.model.server")();
     var productModel = require("./product/product.model.server")();
